2b1872f54438540ed2ac8d9baf973585536099c3
Nerzal <theel.tobias@gmx.de>
2015-03-17 22:10:34 +0100

    Initial commit

---
+++ LibGDX/android/src/com/kassel/codemeetup/demo/android/AndroidLauncher.java
@@ -0,0 +1,16 @@
+package com.kassel.codemeetup.demo.android;
+
+import android.os.Bundle;
+
+import com.badlogic.gdx.backends.android.AndroidApplication;
+import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
+import com.kassel.codemeetup.demo.MyCodeMeetupGame;
+
+public class AndroidLauncher extends AndroidApplication {
+	@Override
+	protected void onCreate (Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
+		initialize(new MyCodeMeetupGame(), config);
+	}
+}
